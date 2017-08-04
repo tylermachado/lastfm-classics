f8df574980d000b076c08c1a5b7a484a75565167
tyler-machado <tyler.machado@hbr.org>
2017-08-04 10:28:10 -0400

    ready for Slack

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var request	=	require("request"),
 	fs = 		require('fs'),
 	moment =	require('moment'),
-	nodemailer = require("nodemailer"),
 	dotenv =	require('dotenv').config(),
 	express=	require('express'),
-	app =		express();
+	app =		express(),
+	Slack = require('slack-node');
 
 var user = process.env.USERNAME,
 	apiKey = process.env.APIKEY,
@@ -20,23 +20,38 @@ var url = "http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart" +
 			"&to=" + pastTo + 
 			"&api_key=" + apiKey + 
 			"&format=json";
+			 
+slack = new Slack();
+slack.setWebhook(process.env.SLACK_WEBHOOK);
 
 request(url, function (error, response, body) {
 	if (!error) {
     
-    var list = JSON.parse(body);
-    
-    list = list.weeklyalbumchart.album;
-    
-    var array = [];
-    
-    for (var i=0; i<10; i++) {
-      array.push(list[i].artist["#text"] + ", " + list[i].name)
-    }
-    
+	    var list = JSON.parse(body);
+	    
+	    list = list.weeklyalbumchart.album;
+	    
+	    var array = [];
+	    
+	    for (var i=0; i<10; i++) {
+	    	var obj = new Object();
+	    	obj.text = (i+1) + ". " + list[i].artist["#text"] + ", " + list[i].name;
+	      	array.push(obj);
+	    }
+
+	    slack.webhook({
+	      channel: "#general",
+	      username: "Last.fm Classics",
+	      icon_emoji: ":musical_note:",
+	      text: "Here's what you listened to this week 10 years ago:",
+	      attachments: array
+	    }, function(err, response) {
+	      console.log(response);
+	    });
+	    
 		app.get('/', function (req, res) {
-      res.send(array);
-    });
+	      res.send(array);
+	    });
 	} else {
 		console.log("We’ve encountered an error: " + error);
 	}
